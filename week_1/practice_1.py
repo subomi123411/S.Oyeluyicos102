d194238c3fcc63095617eb9a4f347b9d42b3f591
subomi123411 <subomioyeluyi@gmail.com>
2025-03-19 00:36:38 +0100

    first commit

---
+++ week_1/practice_1.py
@@ -0,0 +1,15 @@
+# Integer number
+num = 100
+print(num)
+print("Data type of variable num is", type(num))
+
+# float number
+fnum = 34.45
+print(fnum)
+print("Data type of variable fnum is", type(fnum))
+
+# complex number
+cnum = 3 + 4j
+print(cnum)
+print("Data type of variable cnum is", type(cnum))
+
---
+++ week_1/practice_2.py
@@ -0,0 +1,11 @@
+# Python program to print strings and type
+str1 = "HI my name is Matthew. I am String."
+str2 = 'Hi my name is Precious. I am also a string.'
+
+# display string str1 and its type
+print(str1)
+print(type(str1))
+
+# display string str2 and its type
+print(str2)
+print(type(str2))
